ca19012a782d7da62d80a6bcd9c9d91b2438aabf
Mehul721 <jmehul721@gmail.com>
2024-08-08 22:51:06 +0530

    Blinkit Assignment Part-1

---
+++ blinkitAssignment/ProductGridView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductGridView.swift
+//  blinkitAssignment
+//
+//  Created by Mehul Jain on 08/08/24.
+//
+
+import SwiftUI
+
+struct ProductGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductGridView()
+}
---
+++ blinkitAssignment/ProductItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductItemView.swift
+//  blinkitAssignment
+//
+//  Created by Mehul Jain on 08/08/24.
+//
+
+import SwiftUI
+
+struct ProductItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductItemView()
+}
