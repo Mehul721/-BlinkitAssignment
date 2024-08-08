efce48a0928961bc064d1343c26b2d3d8f0651f4
Mehul721 <jmehul721@gmail.com>
2024-08-08 17:45:15 +0530

    Initial Commit

---
+++ blinkitAssignment/blinkitAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  blinkitAssignmentApp.swift
+//  blinkitAssignment
+//
+//  Created by Mehul Jain on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct blinkitAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
